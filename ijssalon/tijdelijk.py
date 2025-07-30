40da8fa5cfe8d4663c122ce9c3e63210d4ab2882
kathy <koobbeee@hotmail.com>
2025-07-30 13:18:28 +0200

    bestand helper.py toegevoegd

---
+++ ijssalon/tijdelijk.py
@@ -0,0 +1,16 @@
+mijn_prijzen = {
+    'aardbei' : '3',
+    'vanille': '4',
+    'chocolade' : '5'
+}
+
+aanbieding = ['aardbei'] * 0.8
+
+reclame_tekst = Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € <aanbieding>.
+aanbieding = '4'
+print (f'Vandaag in de aanbieding: vanille-ijs, 1 liter – slechts € <aanbieding>.')
+reclame_tekst2 = integer
+reclame_tekst3 = print('reclame_tekst').upper()
+reclame_tekst4 = 'Vandaag, in, de, aanbieding:, vanille-ijs, 1 liter, – slechts, € <aanbieding>.'
+
+print('reclame_tekst').lower()
