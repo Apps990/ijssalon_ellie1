910d9847db0cdfde9e7cb5564206ccb3fe585d11
Apps990 <106185974+Apps990@users.noreply.github.com>
2025-08-03 16:28:47 +0200

    Update tijdelijk.py

--- ijssalon/tijdelijk.py
+++ ijssalon/tijdelijk.py
@@ -1,45 +1,39 @@
-mijn_prijzen = {
+prijzen = {
     'aardbei' : '3',
     'vanille': '4',
     'chocolade' : '5'
 }
 
-aanbieding = 3 * 0.8
+aanbieding = prijzen['aardbei'] * 0.8
 
-reclame_tekst = 'Vandaag in de aanbieding: vanille-ijs, 1 liter  slechts € {aanbieding}'
+reclame_tekst = f"'Vandaag in de aanbieding: vanille-ijs, 1 liter  slechts € {aanbieding}"
 
 
 
-reclame_tekst2 = round(aanbieding, 2)
-'Vandaag in de aanbieding: vanille-ijs, 1 liter  slechts € {aanbieding}'
+reclame_tekst2 = reclame_tekst[:63]
 
-reclame_tekst3 = ('Vandaag in de aanbieding: vanille-ijs, 1 liter  slechts {aanbieding}').upper()
-reclame_tekst4 = ['Vandaag', 'in', 'de', 'aanbieding:', 'vanille-ijs', '1 liter',  'slechts', '€ <aanbieding>']
-def verwerk_string(reclame_tekst):
-    """
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+
+"""
     Verwerkt een string en print deze met hoofdletters of kleine letters,
     afhankelijk van de lengte.
     """
-    for woord in reclame_tekst.split():
-        if len(woord) == 5:
-            print(woord.upper())
-        elif len(woord) == 4:
-            print(woord.lower())
-        else:
-            print(woord)
-
-# Voorbeeldgebruik
-reclame_tekst4 = 'Vandaag in de aanbieding: vanille-ijs, 1 liter  slechts € aanbieding'
-verwerk_string(reclame_tekst4)
+    for el in reclame_tekst4
+        if len(el) > 4:
+            print(el.upper())
+        else 
+            print(el.lower())
+    
 
 for el in reclame_tekst4:
 
     el.lower()
 
 
-def format_string(input_string):
+def format_string(reclame_tekst4):
 
-    if len(input_string) >= 5:
-        return input_string[:100].upper() 
-    elif len(input_string) == 4:
-        return input_string.lower()
+    if len(reclame_tekst4) >= 5:
+        return reclame_tekst4[:100].upper() 
+    elif len(reclame_tekst4) == 4:
+        return reclame_tekst4.lower()
